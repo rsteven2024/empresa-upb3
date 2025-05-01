93c577390c0a4ac27a60008d86eba16bf2badcb7
rsteven2024 <rstevenlc@gmail.com>
2025-04-30 20:01:20 -0500

    Creacion de los archivos de empresa

---
+++ src/main/java/com/computadores_upb3/EmpresaUpb3Application.java
@@ -0,0 +1,13 @@
+package com.computadores_upb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaUpb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaUpb3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/computadores_upb3/EmpresaUpb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computadores_upb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaUpb3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
